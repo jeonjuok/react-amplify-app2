ddd828e00b5074fd2fbb47fefb501fd933152836
jeonjuok <medikosoft@gmail.com>
2021-01-30 00:07:53 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React, { useState } from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
 import logo from './logo.svg';
 import './App.css';
+
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
+
 
 function App() {
   return (
@@ -19,7 +44,28 @@ function App() {
           Learn React
         </a>
       </header>
+    
+    
+      <Container className="p-3">
+        <Jumbotron>
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <ExampleToast>
+            We now have Toasts
+            <span role="img" aria-label="tada">
+              🎉
+            </span>
+          </ExampleToast>
+        </Jumbotron>
+      </Container>        
+    
+    
+    
+    
     </div>
+    
+    
+
+    
   );
 }
 
